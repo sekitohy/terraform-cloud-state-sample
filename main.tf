808f5b31ceb661d4f3d0998e632be5238730fc0e
sekitohy <119919407+sekitohy@users.noreply.github.com>
2022-12-06 15:56:42 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-1"
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "example_sekito" {
   cidr_block = "10.0.0.0/16"
 }
